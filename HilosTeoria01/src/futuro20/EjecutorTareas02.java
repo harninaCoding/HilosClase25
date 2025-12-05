023978d9d9781cc30e650691222644a48aff8d29
programacion WEB <jasanchezm01@iesarroyoharnina.es>
2025-12-05 09:06:08 +0100

    repaso teoria

--- HilosTeoria01/src/futuro20/EjecutorTareas02.java
+++ HilosTeoria01/src/futuro20/EjecutorTareas02.java
@@ -1,11 +1,12 @@
 package futuro20;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class EjecutorTareas02 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		// crea y nombra cada objeto Runnable
 		TareaImprimir01 tarea1 = new TareaImprimir01("tarea1",3000);
 		TareaImprimir01 tarea2 = new TareaImprimir01("tarea2",1000);
@@ -16,6 +17,10 @@ public class EjecutorTareas02 {
 		Future<String> submitResponse = ejecutorSubprocesos.submit(tarea1);
 		ejecutorSubprocesos.submit(tarea2);
 		ejecutorSubprocesos.submit(tarea3);
+		//Aqui el main no espera el resultado
+		//		System.out.println(submitResponse);
+		//Aqui el main SI espera el resultado
+		//		System.out.println(submitResponse.get());
 		ejecutorSubprocesos.execute(new Recogedor02(submitResponse));
 		ejecutorSubprocesos.shutdown();
 		
